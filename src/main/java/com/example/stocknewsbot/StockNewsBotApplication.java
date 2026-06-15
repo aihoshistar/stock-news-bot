c5ae58dae2d245e19685b354800c718c64ad59e1
aihoshistar <troublesome.dev@gmail.com>
2026-06-15 15:47:13 +0900

    init spring boot 4.1

---
+++ src/main/java/com/example/stocknewsbot/StockNewsBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.stocknewsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockNewsBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockNewsBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stocknewsbot/StockNewsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stocknewsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockNewsBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
